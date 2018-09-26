afbb6b2d69f20d4a9f1855fa2df078c816472835
Sahil Saini <sahil@akosweb.com>
2018-09-26 10:55:19 -0700

    Building Terms Changelog Test Run

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -6,5 +6,8 @@ echo -e "\e[1;31mBuild Jekyll for Pandoc to make new ToS Docx"
 bundle exec jekyll build
 bundle exec pandoc -s --from html _site/terms/word.html --reference-doc=reference.docx -o Gruntwork-TermsOfService.docx
 echo -e "\e[1;32mNew TOS Document (Gruntwork-TermsOfService.docx) File Generated"
+echo -e "\e[1;31mBuild Terms CHANGELOG.md"
+bundle exec git log -p --pretty=format:'###%h : %s' _data/terms-of-service.yml > pages/terms/CHANGELOG.md
+echo -e "\e[1;32mFinished building CHANGELOG.md"
 echo -e "\e[1;31mRun Jekyll serve to watch for changes"
 bundle exec jekyll serve --drafts --incremental --host 0.0.0.0
